e7fcb0b49cfb318e07bb72d59c03cd9a8c59749b
Hector Palomares <hector.palomares@alteumx.com>
2018-09-18 19:21:05 -0500

    Chapter 5: HOF. Introduction

---
+++ chapter-5-higher-order-functions/chapter-5-general.js
@@ -0,0 +1,60 @@
+// Higher-Order Functions: Functions that operate on other functions, either by taking them as arugments or by returning them. Abstractt over actions, not just values.
+// Abstraction: hide details, give us the ability to see the problems at a higher level.
+function repeat(n, action) {
+  for (let i = 0; i < n; i++) {
+    action(i);
+  }
+}
+
+// Create new functions
+function greaterThan(n) {
+  return m => m > n;
+}
+
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(17));   // true
+console.log(greaterThan10(8));    // false
+
+// Change other functions
+function noisy(f) {
+  return(...args) => {
+    console.log("calling with", args);
+    let result = f(...args);
+    console.log("called with", args, ", returned", result);
+    return result;
+  };
+}
+
+noisy(Math.min)(3, 2, 1, -3);
+// calling with [ 3, 2, 1, -3 ]
+// called with [ 3, 2, 1, -3 ] , returned -3
+
+// functions to provide new types of control flow
+function unless(test, then) {
+  if(!test) then();
+}
+
+repeat(7, n => {
+  unless(n % 2 == 1, () => {
+    console.log(n, "is even");
+  });
+});
+// 0 'is even'
+// 2 'is even'
+// 4 'is even'
+// 6 'is even'
+
+// Filtering Arrays (logic of es6 .filter()): builds up a new array with the elements that pass the test. This is a 'pure' function since it doesn't mutate the given array.
+function myFilter(array, test) {
+  let passed = [];
+  for(let element of array) {
+    if(test(element)) {
+      passed.push(element);
+    }
+  }
+  return passed;
+}
+
+let numbers = [5, 25, 12, 8, 2];
+
+console.log(myFilter(numbers, number => number > 10));  //  [ 25, 12 ]
